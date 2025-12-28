7abf9420d544ecf13d9c7f7ae8b1136a04b8c7fd
march213 <march213@yahoo.com>
2025-12-28 12:18:51 -0500

    sphere setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,121 @@
+import { Canvas, useFrame } from "@react-three/fiber";
+import * as THREE from "three";
+import { Environment, OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+import CustomShaderMaterial from "three-custom-shader-material";
+import { mergeVertices } from "three/examples/jsm/utils/BufferGeometryUtils.js";
+
+import wobbleVertexShader from "./shaders/wobble/vertex.glsl";
+import wobbleFragmentShader from "./shaders/wobble/fragment.glsl";
+
+import { useMemo, useRef } from "react";
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [3, 3, 3] }}>
+        <OrbitControls />
+        <Environment preset="city" />
+        <WobbleSphere />
+        <directionalLight position={[0.25, 2, -2.25]} intensity={1} />
+      </Canvas>
+    </>
+  );
+}
+
+const WobbleSphere = () => {
+  // This reference will give us direct access to the mesh
+  const mesh = useRef(null);
+  const materialRef = useRef(null);
+
+  const {
+    metalness,
+    roughness,
+    transmission,
+    ior,
+    thickness,
+    transparent,
+    wireframe,
+    // uniforms
+    uPositionFrequency,
+    uTimeFrequency,
+    uStrenght,
+    uWarpPositionFrequency,
+    uWarpTimeFrequency,
+    uWarpStrenght,
+    uColorA,
+    uColorB,
+  } = useControls({
+    metalness: { value: 0, min: 0, max: 1, step: 0.001 },
+    roughness: { value: 0.5, min: 0, max: 1, step: 0.001 },
+    transmission: { value: 0, min: 0, max: 1, step: 0.001 },
+    ior: { value: 1.5, min: 0, max: 10, step: 0.001 },
+    thickness: { value: 1.5, min: 0, max: 10, step: 0.001 },
+    transparent: false,
+    wireframe: false,
+
+    // uniforms
+    uPositionFrequency: { value: 0.5, min: 0, max: 2, step: 0.001 },
+    uTimeFrequency: { value: 0.4, min: 0, max: 2, step: 0.001 },
+    uStrenght: { value: 0.3, min: 0, max: 2, step: 0.001 },
+    uWarpPositionFrequency: { value: 0.38, min: 0, max: 2, step: 0.001 },
+    uWarpTimeFrequency: { value: 0.12, min: 0, max: 2, step: 0.001 },
+    uWarpStrenght: { value: 1.7, min: 0, max: 2, step: 0.001 },
+    uColorA: { value: "#ff0000" },
+    uColorB: { value: "#00ff00" },
+  });
+
+  const geometry = useMemo(() => {
+    // 1. Create the base geometry
+    const baseGeom = new THREE.IcosahedronGeometry(2.5, 50);
+
+    // 2. Merge the vertices
+    const mergedGeom = mergeVertices(baseGeom);
+
+    // 3. Compute tangents if you use normal maps in your shader
+    mergedGeom.computeTangents();
+
+    return mergedGeom;
+  }, []);
+
+  useFrame((state) => {
+    if (materialRef.current) {
+      materialRef.current.uniforms.uTime.value = state.clock.elapsedTime;
+    }
+  });
+
+  return (
+    <mesh ref={mesh} geometry={geometry}>
+      <CustomShaderMaterial
+        ref={materialRef}
+        // CSM
+        baseMaterial={THREE.MeshPhysicalMaterial}
+        // custom shader material props
+        vertexShader={wobbleVertexShader}
+        fragmentShader={wobbleFragmentShader}
+        // mesh MeshPhysicalMaterial props
+        metalness={metalness}
+        roughness={roughness}
+        transmission={transmission}
+        ior={ior}
+        thickness={thickness}
+        transparent={transparent}
+        wireframe={wireframe}
+        //uniforms
+        uniforms={{
+          uTime: { value: 0 },
+          uPositionFrequency: { value: uPositionFrequency },
+          uTimeFrequency: { value: uTimeFrequency },
+          uStrenght: { value: uStrenght },
+          uWarpPositionFrequency: { value: uWarpPositionFrequency },
+          uWarpTimeFrequency: { value: uWarpTimeFrequency },
+          uWarpStrenght: { value: uWarpStrenght },
+          uColorA: { value: new THREE.Color(uColorA) },
+          uColorB: { value: new THREE.Color(uColorB) },
+        }}
+      />
+    </mesh>
+  );
+};
+
+export default App;
---
+++ frontend/src/shaders/wobble/vertex.glsl
@@ -0,0 +1,52 @@
+attribute vec4 tangent;
+
+#include ../simplexNoise4d.glsl
+
+uniform float uTime;
+uniform float uPositionFrequency;
+uniform float uTimeFrequency;
+uniform float uStrenght;
+uniform float uWarpPositionFrequency;
+uniform float uWarpTimeFrequency;
+uniform float uWarpStrenght;
+
+varying float vWobble;
+
+float getWobble(vec3 position) {
+    vec3 warpedPosition = position;
+
+    warpedPosition += simplexNoise4d(vec4(
+                warpedPosition * uWarpPositionFrequency, // XYZ
+                uTime * uWarpTimeFrequency // W
+            )) * uWarpStrenght;
+
+    return simplexNoise4d(vec4(
+            warpedPosition * uPositionFrequency, // XYZ
+            uTime * uTimeFrequency // W
+        )) * uStrenght;
+    ;
+}
+
+void main() {
+    vec3 biTangent = cross(normal, tangent.xyz);
+
+    // Neighbours positions
+    float shift = 0.01;
+    vec3 positionA = csm_Position + tangent.xyz * shift;
+    vec3 positionB = csm_Position + biTangent * shift;
+
+    // Wobble
+    float wobble = getWobble(csm_Position);
+    csm_Position += wobble * normal;
+    positionA += getWobble(positionA) * normal;
+    positionB += getWobble(positionB) * normal;
+
+    // compute normal
+    vec3 toA = normalize(positionA - csm_Position);
+    vec3 toB = normalize(positionB - csm_Position);
+    csm_Normal = cross(toA, toB);
+
+    // varyings
+    // vWobble = wobble / uStrenght;
+    vWobble = wobble / uStrenght;
+}
